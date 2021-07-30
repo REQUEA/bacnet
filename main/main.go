1511afb0a5bc4408ad6c6d7055bfbb3573187d41
Aurelien Deroide <aderoide@requea.com>
2021-07-30 16:23:48 +0200

    Don't lint main

--- main/main.go
+++ main/main.go
@@ -1,4 +1,3 @@
-//nolint: deadcode, unused
 package main
 
 import (
@@ -36,7 +35,7 @@ func main() {
 		log.Fatal("whois: ", err)
 	}
 	fmt.Printf("%+v\n", devices)
-	err = listObjects(c, devices[0])
+	//err = listObjects(c, devices[0])
 	// err = readValue(c, devices[0], types.ObjectID{
 	// 	Type:     types.Schedule,
 	// 	Instance: 1,
